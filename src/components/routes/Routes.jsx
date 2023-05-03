0c74ccd64bf8093f103a686a68b2bb70113ac978
mehedi hasan <mdchanchal5675@gmail.com>
2023-05-04 00:21:11 +0600

    Footer section completed

--- src/components/routes/Routes.jsx
+++ src/components/routes/Routes.jsx
@@ -6,6 +6,7 @@ import Main from "../layout/Main/Main";
 import Blog from "../pages/Blog/Blog";
 import ChefRecipe from "../pages/ChefRecipe/ChefRecipe/ChefRecipe";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
 export const router = createBrowserRouter([
     {
       path: "/",
@@ -22,6 +23,10 @@ export const router = createBrowserRouter([
         path:'chef-recipes/:id',
         element:<ChefRecipe></ChefRecipe>,
         loader:({params})=>fetch(`https://chef-recipe-hunter-server-side-lilac.vercel.app/chef/${params.id}`)
+      },
+      {
+        path:'/login',
+        element:<Login></Login>
       }
     ]
       
