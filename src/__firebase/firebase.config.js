db1789faf5045d7892fff3c3d3c67cd206799685
mehedi hasan <mdchanchal5675@gmail.com>
2023-05-04 19:32:26 +0600

    use Environment variables Done

--- src/__firebase/firebase.config.js
+++ src/__firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC7bgfpuRK7d7sCOz38bRdpvlDwcDwIAlk",
-  authDomain: "the-chef-recipes-client-6b1af.firebaseapp.com",
-  projectId: "the-chef-recipes-client-6b1af",
-  storageBucket: "the-chef-recipes-client-6b1af.appspot.com",
-  messagingSenderId: "182249467783",
-  appId: "1:182249467783:web:551d79d2b8a968155b6d22"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
